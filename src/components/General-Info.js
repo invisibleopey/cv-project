92e09aaf71e9d53c30acb6c0cd8e5e15af9e8b70
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-10-10 17:55:46 +0100

    Remove labels and use placeholder instead

--- src/components/General-Info.js
+++ src/components/General-Info.js
@@ -6,36 +6,48 @@ function GeneralInfo(props) {
     <div>
       <h2>General Information</h2>
       <form action="#" className="gen-info">
-        <label htmlFor="firstName">
-          First Name:
-          <input type="text" name="firstName" value={genInfo.firstName} onChange={handleChange} />
-        </label>
-        <label htmlFor="lastName">
-          Last Name:
-          <input type="text" name="lastName" value={genInfo.lastName} onChange={handleChange} />
-        </label>
-        <label htmlFor="title">
-          Title:
-          <input type="text" name="title" value={genInfo.title} onChange={handleChange} />
-        </label>
-        <label htmlFor="address">
-          Address:
-          <input type="text" name="address" value={genInfo.address} onChange={handleChange} />
-        </label>
-        <label htmlFor="email">
-          Email:
-          <input type="email" name="email" value={genInfo.email} onChange={handleChange} />
-        </label>
-        <label htmlFor="phoneNumber">
-          Phone Number:
-          <input
-            type="text"
-            name="phoneNumber"
-            value={genInfo.phoneNumber}
-            onChange={handleChange}
-          />
-        </label>
-        <button type="submit">Submit</button>
+        <input
+          type="text"
+          name="firstName"
+          value={genInfo.firstName}
+          onChange={handleChange}
+          placeholder="First Name"
+        />
+        <input
+          type="text"
+          name="lastName"
+          value={genInfo.lastName}
+          onChange={handleChange}
+          placeholder="Last Name"
+        />
+        <input
+          type="text"
+          name="title"
+          value={genInfo.title}
+          onChange={handleChange}
+          placeholder="Title"
+        />
+        <input
+          type="text"
+          name="address"
+          value={genInfo.address}
+          onChange={handleChange}
+          placeholder="Address"
+        />
+        <input
+          type="email"
+          name="email"
+          value={genInfo.email}
+          onChange={handleChange}
+          placeholder="Email"
+        />
+        <input
+          type="text"
+          name="phoneNumber"
+          value={genInfo.phoneNumber}
+          onChange={handleChange}
+          placeholder="Phone Number"
+        />
       </form>
     </div>
   );
