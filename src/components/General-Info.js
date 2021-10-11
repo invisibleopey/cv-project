adfa0e0300dc11596ee882bca45d239d435081fc
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-10-12 00:02:49 +0100

    Refactor code, remove deep rooted state, add textarea for description

--- src/components/General-Info.js
+++ src/components/General-Info.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 function GeneralInfo(props) {
-  const { genInfo, handleChange } = props;
+  const { handleChange, firstName, lastName, title, address, email, phoneNumber, description } =
+    props;
   return (
     <div>
       <h2>General Information</h2>
@@ -9,45 +10,47 @@ function GeneralInfo(props) {
         <input
           type="text"
           name="firstName"
-          value={genInfo.firstName}
+          value={firstName}
           onChange={handleChange}
           placeholder="First Name"
         />
         <input
           type="text"
           name="lastName"
-          value={genInfo.lastName}
+          value={lastName}
           onChange={handleChange}
           placeholder="Last Name"
         />
-        <input
-          type="text"
-          name="title"
-          value={genInfo.title}
-          onChange={handleChange}
-          placeholder="Title"
-        />
+        <input type="text" name="title" value={title} onChange={handleChange} placeholder="Title" />
         <input
           type="text"
           name="address"
-          value={genInfo.address}
+          value={address}
           onChange={handleChange}
           placeholder="Address"
         />
         <input
           type="email"
           name="email"
-          value={genInfo.email}
+          value={email}
           onChange={handleChange}
           placeholder="Email"
         />
         <input
           type="text"
           name="phoneNumber"
-          value={genInfo.phoneNumber}
+          value={phoneNumber}
           onChange={handleChange}
           placeholder="Phone Number"
         />
+        <textarea
+          name="description"
+          cols="10"
+          rows="10"
+          placeholder="Description"
+          value={description}
+          onChange={handleChange}
+        ></textarea>
       </form>
     </div>
   );
