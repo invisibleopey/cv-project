ba3d1e49040e494c29c36463176b2c947d99af87
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-10-12 00:51:06 +0100

    Instantiate education form input with hardly typed values

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,7 +1,56 @@
 import React from 'react';
 
-function Education() {
-  return <div>Education Section</div>;
+function Education(props) {
+  return (
+    <div>
+      <h2>Education</h2>
+      <form action="#" className="education">
+        <input
+          type="text"
+          name="name"
+          value="University of Ilorin"
+          // onChange={handleChange}
+          placeholder="University Name"
+        />
+        <input
+          type="text"
+          name="city"
+          value="Ilorin"
+          // onChange={handleChange}
+          placeholder="City"
+        />
+        <input
+          type="text"
+          name="degree"
+          value="MBBS"
+          // onChange={handleChange}
+          placeholder="Degree"
+        />
+        <input
+          type="text"
+          name="subject"
+          value="Medicine and Surgery"
+          // onChange={handleChange}
+          placeholder="Subject"
+        />
+        <input
+          type="text"
+          name="startDate"
+          value="2015"
+          // onChange={handleChange}
+          placeholder="From"
+        />
+        <input
+          type="text"
+          name="endDate"
+          value="2022"
+          // onChange={handleChange}
+          placeholder="To"
+        />
+        <button type="submit">ADD</button>
+      </form>
+    </div>
+  );
 }
 
 export default Education;
