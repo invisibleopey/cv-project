ca5fd686f23b20f2cc6ca940c25b7be5f6bac034
Abdullahi Muritala <oabdullahi37@gmail.com>
2021-10-14 15:46:37 +0100

    Render the form inputs with values set to imported prop values

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
-function Experience() {
-  return <div>Experience Section</div>;
+function Experience(props) {
+  const { job } = props;
+  return (
+    <div>
+      <h2>Experience</h2>
+      <form action="#" className="experience">
+        <input type="text" name="position" value={job.position} placeholder="Position" />
+        <input type="text" name="company" value={job.company} placeholder="Company" />
+        <input type="text" name="city" value={job.city} placeholder="City" />
+        <input type="text" name="startDate" value={job.startDate} placeholder="From" />
+        <input type="text" name="endDate" value={job.endDate} placeholder="To" />
+        <button type="submit">ADD</button>
+      </form>
+    </div>
+  );
 }
 
 export default Experience;
